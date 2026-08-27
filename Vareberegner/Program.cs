1049991fb61b79cfa0c438760ad3b987896095d1
Sujipals <sujithamar08@gmail.com>
2026-08-27 12:53:45 +0200

    Tilføj indtastning af varelinje

--- Vareberegner/Program.cs
+++ Vareberegner/Program.cs
@@ -2,8 +2,21 @@
 
 class Program
 {
-	static void Main(string[] args)
-	{
-		Console.WriteLine("=== Vareberegner ===");
-	}
+    static void Main(string[] args)
+    {
+        Console.WriteLine("=== Vareberegner ===");
+
+        Console.Write("Indtast varenavn: ");
+        string productName = Console.ReadLine();
+
+        Console.Write("Indtast antal: ");
+        int quantity = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Indtast pris pr. enhed: ");
+        decimal unitPrice = Convert.ToDecimal(Console.ReadLine());
+
+        Console.WriteLine($"Vare: {productName}");
+        Console.WriteLine($"Antal: {quantity}");
+        Console.WriteLine($"Pris pr. enhed: {unitPrice:F2} kr.");
+    }
 }
