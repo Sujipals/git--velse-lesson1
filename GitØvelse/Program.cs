f691e6c01fc07e5a9de4485808f64039c2a783b6
Sujipals <sujithamar08@gmail.com>
2026-08-27 13:52:11 +0200

    Tilføj personlig hilsen

--- GitØvelse/Program.cs
+++ GitØvelse/Program.cs
@@ -1,1 +1,12 @@
-﻿Console.WriteLine("I am learning Git and C#.");
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Indtast dit navn: ");
+        string name = Console.ReadLine();
+
+        Console.WriteLine($"Hej {name}!");
+    }
+}
